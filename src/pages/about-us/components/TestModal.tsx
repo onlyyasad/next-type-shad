c3da60971dddede8ef26013ddd765771fc98708a
Hafij Al Asad <onlyyasad@gmail.com>
2023-08-21 00:05:16 +0600

    custom combobox using select

--- src/pages/about-us/components/TestModal.tsx
+++ src/pages/about-us/components/TestModal.tsx
@@ -8,12 +8,27 @@ import {
     SelectTrigger,
     SelectValue,
 } from "@/components/ui/select"
+import { ComboboxDemo } from '@/components/ui/combobox';
+import { Input } from '@/components/ui/input';
+import {
+    Command,
+    CommandDialog,
+    CommandEmpty,
+    CommandGroup,
+    CommandInput,
+    CommandItem,
+    CommandList,
+    CommandSeparator,
+    CommandShortcut,
+} from "@/components/ui/command"
+import { ScrollArea } from '@/components/ui/scroll-area';
 
-const TestModal = ({open, setOpen}) => {
+
+const TestModal = ({ open, setOpen }) => {
     const [value, setValue] = useState("light")
     console.log(value);
     return (
-        <Dialog open={open} onOpenChange={setOpen}>
+        <Dialog open={open} onOpenChange={setOpen} className="h-[500px]">
             <DialogTrigger asChild>Open</DialogTrigger>
             <DialogContent>
                 <DialogHeader>
@@ -23,16 +38,57 @@ const TestModal = ({open, setOpen}) => {
                         and remove your data from our servers.
                     </DialogDescription>
                 </DialogHeader>
-                <Select onValueChange={setValue}>
-                    <SelectTrigger className="w-[180px]">
-                        <SelectValue placeholder="Theme" />
-                    </SelectTrigger>
-                    <SelectContent>
-                        <SelectItem value="Light">Light</SelectItem>
-                        <SelectItem value="Dark">Dark</SelectItem>
-                        <SelectItem value="System">System</SelectItem>
-                    </SelectContent>
-                </Select>
+                <div>
+                    <Select onValueChange={setValue}>
+                        <SelectTrigger className="w-[180px]">
+                            <SelectValue placeholder="Theme" />
+                            {/* <input type="text" placeholder="Theme" /> */}
+                            {/* <Input type="text" placeholder="Theme" /> */}
+                        </SelectTrigger>
+                        <SelectContent>
+                            <Command>
+                                <CommandInput placeholder="Type a command or search..." />
+                                <CommandList>
+                                    <CommandEmpty>No results found.</CommandEmpty>
+                                    <CommandGroup heading="Suggestions">
+                                        <ScrollArea className='h-32'>
+                                            <CommandItem className="p-0">
+                                                <SelectItem value="Light">Light</SelectItem>
+                                            </CommandItem>
+                                            <CommandItem className="p-0">
+                                                <SelectItem value="Dark">Dark</SelectItem>
+                                            </CommandItem>
+                                            <CommandItem className="p-0">
+                                                <SelectItem value="System">System</SelectItem>
+                                            </CommandItem>
+                                            <CommandItem className="p-0">
+                                                <SelectItem value="Systemm">Systemm</SelectItem>
+                                            </CommandItem>
+                                            <CommandItem className="p-0">
+                                                <SelectItem value="Sstem">Sstem</SelectItem>
+                                            </CommandItem>
+                                            <CommandItem className="p-0">
+                                                <SelectItem value="Sytem">Sytem</SelectItem>
+                                            </CommandItem>
+                                            <CommandItem className="p-0">
+                                                <SelectItem value="Sysem">Sysem</SelectItem>
+                                            </CommandItem>
+                                            <CommandItem className="p-0">
+                                                <SelectItem value="Systm">Systm</SelectItem>
+                                            </CommandItem>
+                                            <CommandItem className="p-0">
+                                                <SelectItem value="Syste">Syste</SelectItem>
+                                            </CommandItem>
+                                        </ScrollArea>
+                                    </CommandGroup>
+                                </CommandList>
+                            </Command>
+                        </SelectContent>
+                    </Select>
+                </div>
+                <div>
+                    <ComboboxDemo />
+                </div>
             </DialogContent>
         </Dialog>
     );
